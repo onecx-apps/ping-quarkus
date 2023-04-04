35eb994d1447175d73f1658b800df620ebc6bc62
Andrej Petras <andrej@lorislab.org>
2023-04-04 16:22:05 +0200

    chore(ci): create ping quarkus project

---
+++ src/test/java/org/tkit/quarkus/hello/rs/HelloRestControllerTestIT.java
@@ -0,0 +1,7 @@
+package org.tkit.quarkus.hello.rs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloRestControllerTestIT extends HelloRestControllerTest {
+}
